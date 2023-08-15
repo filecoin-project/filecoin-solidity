fe167d79a22411cc7acea8079f9ee493bee55467
bojinovic <milos.bojinovic@mvpworkshop.co>
2023-08-15 17:32:36 +0200

    cleanup

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,8 +7,6 @@ try {
     const tomlData = fs.readFileSync("./foundry.toml", { encoding: "utf8", flag: "r" })
 
     extractedSolcVersion = tomlData.split(`solc`)[1].split("=")[1].split("\n")[0].replaceAll(" ", "").replaceAll('"', "").replaceAll("'", "")
-
-    console.log({ extractedSolcVersion })
 } catch {
     console.log({ error: "Solc version in foundry.toml not set" })
     process.exit(1)
