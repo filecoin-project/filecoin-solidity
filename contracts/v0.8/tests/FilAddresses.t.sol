7a1fb7a5d28c342b1f13c2d54ef8aaa87d279b42
vonum <vonum.mk@gmail.com>
2023-09-06 11:39:26 +0200

    Fuzz all tests for fil address lib

--- contracts/v0.8/tests/FilAddresses.t.sol
+++ contracts/v0.8/tests/FilAddresses.t.sol
@@ -8,57 +8,31 @@ import {CommonTypes} from "contracts/v0.8/types/CommonTypes.sol";
 contract FilAddressesTest is Test {
     error InvalidAddress();
 
-    address internal testAddress = vm.addr(0x7);
-    bytes internal testAddressBytes = abi.encodePacked(testAddress);
-    CommonTypes.FilAddress internal testFilAddress = CommonTypes.FilAddress(
-      abi.encodePacked(hex"040a", testAddress)
-    );
-
-    function testFromEthAddressValid() public {
-        CommonTypes.FilAddress memory filAddress = FilAddresses.fromEthAddress(testAddress);
-        bytes memory filAddressBytes = abi.encodePacked(hex"040a", testAddressBytes);
-        assertEq(filAddressBytes, filAddress.data);
-    }
-
-    function testToEthAddressValid() public {
-        address addr = FilAddresses.toEthAddress(testFilAddress);
-        assertEq(testAddress, addr);
-    }
-
-    function testToEthAddressInvalidFirstByte() public {
+    function testFuzz_toEthAddressInvalidFirstByte(address addr) public {
         CommonTypes.FilAddress memory filAddress = CommonTypes.FilAddress(
-          abi.encodePacked(hex"030a", testAddress)
+          abi.encodePacked(hex"030a", addr)
         );
 
         vm.expectRevert(InvalidAddress.selector);
         FilAddresses.toEthAddress(filAddress);
     }
 
-    function testToEthAddressInvalidSecondByte() public {
+    function testFuzz_toEthAddressInvalidSecondByte(address addr) public {
         CommonTypes.FilAddress memory filAddress = CommonTypes.FilAddress(
-          abi.encodePacked(hex"040b", testAddress)
+          abi.encodePacked(hex"040b", addr)
         );
 
         vm.expectRevert(InvalidAddress.selector);
         FilAddresses.toEthAddress(filAddress);
     }
 
-    function testToEthAddressInvalidBytesLength() public {
+    function testFuzz_toEthAddressInvalidBytesLength(address addr) public {
         CommonTypes.FilAddress memory filAddress = CommonTypes.FilAddress(
-          abi.encodePacked(hex"040b", testAddress, hex"00")
+          abi.encodePacked(hex"040b", addr, hex"00")
         );
 
         vm.expectRevert(InvalidAddress.selector);
         FilAddresses.toEthAddress(filAddress);
-    }
-
-    function testFuzz_fromEthAddress(address addr) public {
-        bytes memory addrBytes = abi.encodePacked(addr);
-        bytes memory filAddressBytes = abi.encodePacked(hex"040a", addrBytes);
-
-        CommonTypes.FilAddress memory filAddress = FilAddresses.fromEthAddress(addr);
-
-        assertEq(filAddressBytes, filAddress.data);
     }
 
     function testFuzz_toEthAddress(address addr) public {
@@ -69,5 +43,14 @@ contract FilAddressesTest is Test {
         address ethAddress = FilAddresses.toEthAddress(filAddress);
 
         assertEq(addr, ethAddress);
+    }
+
+    function testFuzz_fromEthAddress(address addr) public {
+        bytes memory addrBytes = abi.encodePacked(addr);
+        bytes memory filAddressBytes = abi.encodePacked(hex"040a", addrBytes);
+
+        CommonTypes.FilAddress memory filAddress = FilAddresses.fromEthAddress(addr);
+
+        assertEq(filAddressBytes, filAddress.data);
     }
 }
