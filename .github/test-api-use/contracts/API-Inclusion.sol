448eba01ff84e70ee70b963c608d62a05617fe9f
bojinovic <bojinovicc@hotmail.com>
2024-05-22 10:23:22 +0200

    gh actions: cleanup

--- .github/test-api-use/contracts/API-Inclusion.sol
+++ .github/test-api-use/contracts/API-Inclusion.sol
@@ -1,35 +1,33 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.18;
 
+import {FilAddresses} from "filecoin-solidity-api/contracts/v0.8/utils/FilAddresses.sol";
+import {CommonTypes} from "filecoin-solidity-api/contracts/v0.8/types/CommonTypes.sol";
 
-import { FilAddresses } from  "filecoin-solidity-api/contracts/v0.8/utils/FilAddresses.sol";
-import { CommonTypes } from "filecoin-solidity-api/contracts/v0.8/types/CommonTypes.sol";
+import {AccountTypes} from "filecoin-solidity-api/contracts/v0.8/types/AccountTypes.sol";
+import {DataCapTypes} from "filecoin-solidity-api/contracts/v0.8/types/DataCapTypes.sol";
+import {MarketTypes} from "filecoin-solidity-api/contracts/v0.8/types/MarketTypes.sol";
+import {MinerTypes} from "filecoin-solidity-api/contracts/v0.8/types/MinerTypes.sol";
+import {PowerTypes} from "filecoin-solidity-api/contracts/v0.8/types/PowerTypes.sol";
+import {VerifRegTypes} from "filecoin-solidity-api/contracts/v0.8/types/VerifRegTypes.sol";
 
-import { AccountTypes } from "filecoin-solidity-api/contracts/v0.8/types/AccountTypes.sol";
-import { DataCapTypes } from "filecoin-solidity-api/contracts/v0.8/types/DataCapTypes.sol";
-import { MarketTypes } from "filecoin-solidity-api/contracts/v0.8/types/MarketTypes.sol";
-import { MinerTypes } from "filecoin-solidity-api/contracts/v0.8/types/MinerTypes.sol";
-import { PowerTypes } from "filecoin-solidity-api/contracts/v0.8/types/PowerTypes.sol";
-import { VerifRegTypes } from "filecoin-solidity-api/contracts/v0.8/types/VerifRegTypes.sol";
-
-import { AccountAPI } from "filecoin-solidity-api/contracts/v0.8/AccountAPI.sol";
-import { DataCapAPI } from "filecoin-solidity-api/contracts/v0.8/DataCapAPI.sol";
-import { MarketAPI } from "filecoin-solidity-api/contracts/v0.8/MarketAPI.sol";
-import { MinerAPI } from "filecoin-solidity-api/contracts/v0.8/MinerAPI.sol";
-import { PowerAPI } from "filecoin-solidity-api/contracts/v0.8/PowerAPI.sol";
-import { PrecompilesAPI } from "filecoin-solidity-api/contracts/v0.8/PrecompilesAPI.sol";
-import { SendAPI } from "filecoin-solidity-api/contracts/v0.8/SendAPI.sol";
-import { VerifRegAPI } from "filecoin-solidity-api/contracts/v0.8/VerifRegAPI.sol";
+import {AccountAPI} from "filecoin-solidity-api/contracts/v0.8/AccountAPI.sol";
+import {DataCapAPI} from "filecoin-solidity-api/contracts/v0.8/DataCapAPI.sol";
+import {MarketAPI} from "filecoin-solidity-api/contracts/v0.8/MarketAPI.sol";
+import {MinerAPI} from "filecoin-solidity-api/contracts/v0.8/MinerAPI.sol";
+import {PowerAPI} from "filecoin-solidity-api/contracts/v0.8/PowerAPI.sol";
+import {PrecompilesAPI} from "filecoin-solidity-api/contracts/v0.8/PrecompilesAPI.sol";
+import {SendAPI} from "filecoin-solidity-api/contracts/v0.8/SendAPI.sol";
+import {VerifRegAPI} from "filecoin-solidity-api/contracts/v0.8/VerifRegAPI.sol";
 
 contract API_Inclusion {
-    
     //AccountAPI
     function account_authenticate_message(CommonTypes.FilActorId target, AccountTypes.AuthenticateMessageParams memory params) public view {
         AccountAPI.authenticateMessage(target, params);
     }
 
     //AddressAPI
-     function actorid_conversion() public pure {
+    function actorid_conversion() public pure {
         uint64 actorID = 1;
         CommonTypes.FilAddress memory result = FilAddresses.fromActorID(actorID);
     }
@@ -67,7 +65,7 @@ contract API_Inclusion {
     }
 
     //VerifregAPI
-  function remove_expired_claims(
+    function remove_expired_claims(
         VerifRegTypes.RemoveExpiredClaimsParams memory params
     ) public returns (int256, VerifRegTypes.RemoveExpiredClaimsReturn memory) {
         VerifRegAPI.removeExpiredClaims(params);
